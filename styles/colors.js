7f4af3521d2f2982c111051c63a611a439eec57a
n9heva00 <n9heva00@students.oamk.fi>
2023-04-21 13:48:07 +0300

    Colors

---
+++ styles/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: '#0A8BC2',
+  secondary: '#009978',
+  inactive: '#808080',
+  outline: '#EEEEEE',
+  outlineDark: '#DBDBDB',
+  button: '#35A9DB',
+  textPrimary: '#4D4D4D',
+  textSecondary: '#7D7D7D',
+  danger: '#A94442',
+  tag1: '#F1F5F8',
+  tag2: '#FAD6D4',
+  tag3: '#D4EFFA',
+};
