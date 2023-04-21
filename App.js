ac7be1f24047b2a3fc19c86a72fe56e50a303ea0
Haatis <joona00@windowslive.com>
2023-04-21 13:29:36 +0300

    alapalkki

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import HomeScreen from './Screens/HomeScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+import ProfileScreen from './Screens/ProfileScreen';
+import AlertScreen from './Screens/AlertsScreen';
+import FavoritesScreen from './Screens/FavoritesScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch (route.name) {
+              case 'Etusivu':
+                iconName = focused ? 'home' : 'home-outline';
+                break;
+              case 'Suosikit':
+                iconName = focused ? 'heart' : 'heart-outline';
+                break;
+              case 'Profiili':
+                iconName = focused ? 'account' : 'account-outline';
+                break;
+              case 'Ilmoitukset':
+                iconName = focused ? 'bell' : 'bell-outline';
+                break;
+              case 'Lisää':
+                iconName = focused ? 'plus' : 'plus-outline';
+                break;
+              default:
+                iconName = 'help';
+                break;
+            }
+
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            display: 'flex',
+          },
+        })}
+      >
+        <Tab.Screen name="Etusivu" component={HomeScreen} />
+        <Tab.Screen name="Suosikit" component={FavoritesScreen} />
+        <Tab.Screen name="Profiili" component={ProfileScreen} />
+        <Tab.Screen name="Ilmoitukset" component={AlertScreen} />
+        <Tab.Screen name="Lisää" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
