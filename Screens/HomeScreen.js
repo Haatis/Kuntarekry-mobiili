6911f34019cb4c92a34176c35bee16ffb98d3f16
Haatis <joona00@windowslive.com>
2023-04-21 14:06:42 +0300

    yläpalkki

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,9 +1,28 @@
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Tab, Text, TabView } from '@rneui/themed';
+import { FlatList } from 'react-native';
 
-export default function HomeScreen() {
+export default function FavoritesScreen() {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <Tab
+        value={activeTab}
+        onChange={(e) => setActiveTab(e)}
+        indicatorStyle={{
+          backgroundColor: 'white',
+          height: 3,
+        }}
+        style={{ backgroundColor: 'green' }}
+      >
+        <Tab.Item title="home" titleStyle={{ fontSize: 12, color: 'white' }} />
+        <Tab.Item title="asd" titleStyle={{ fontSize: 12, color: 'white' }} />
+      </Tab>
+      <TabView value={activeTab} onChange={(e) => setActiveTab(e)} animationType="spring">
+        <TabView.Item style={{ width: '100%' }} />
+        <TabView.Item style={{ width: '100%' }} />
+      </TabView>
+    </>
   );
 }
