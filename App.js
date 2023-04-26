b122a7d5c4c7e1e4fbdcf12c8656645e23c11e7d
Haatis <joona00@windowslive.com>
2023-04-26 09:04:30 +0300

    header

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import bgImage from './assets/Subtract.png';
 import { Image } from 'react-native';
 import { useFonts } from 'expo-font';
 import { theme } from './styles/theme';
+import { StatusBar } from 'expo-status-bar';
 import KuntaRekrySVG from './assets/logo.png';
 
 const Tab = createBottomTabNavigator();
@@ -28,86 +29,90 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color }) => {
-            let iconName;
+    <>
+      <StatusBar style="inverted" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color }) => {
+              let iconName;
 
-            switch (route.name) {
-              case 'Etusivu':
-                iconName = focused ? 'home-variant' : 'home-variant-outline';
-                break;
-              case 'Suosikit':
-                iconName = focused ? 'heart' : 'heart-outline';
-                break;
-              case 'Profiili':
-                iconName = focused ? 'account' : 'account-outline';
-                break;
-              case 'Ilmoitukset':
-                iconName = focused ? 'bell' : 'bell-outline';
-                break;
-              case 'Lisää':
-                iconName = 'dots-vertical';
-                break;
-              default:
-                iconName = 'help';
-                break;
-            }
+              switch (route.name) {
+                case 'Etusivu':
+                  iconName = focused ? 'home-variant' : 'home-variant-outline';
+                  break;
+                case 'Suosikit':
+                  iconName = focused ? 'heart' : 'heart-outline';
+                  break;
+                case 'Profiili':
+                  iconName = focused ? 'account' : 'account-outline';
+                  break;
+                case 'Ilmoitukset':
+                  iconName = focused ? 'bell' : 'bell-outline';
+                  break;
+                case 'Lisää':
+                  iconName = 'dots-vertical';
+                  break;
+                default:
+                  iconName = 'help';
+                  break;
+              }
 
-            return <MaterialCommunityIcons name={iconName} size={24} color={color} />;
-          },
-          tabBarLabelStyle: theme.textVariants.ui,
-          tabBarActiveTintColor: theme.colors.primary,
-          tabBarInactiveTintColor: theme.colors.inactive,
-          tabBarStyle: {
-            display: 'flex',
-            height: 54,
-            paddingVertical: 8,
-            paddingBottom: 4,
-            backgroundColor: '#fff',
-          },
-          headerTitleAlign: 'center',
-          headerTitle: (props) => (
-            <View style={{ flex: 1, justifyContent: 'center', alignSelf: 'center' }}>
-              <Image
-                source={KuntaRekrySVG}
-                resizeMode="contain"
-                style={{ width: 215, height: 44 }}
-              />
-            </View>
-          ),
-          headerBackground: () => {
-            return (
-              <View style={{ flex: 1 }}>
-                <LinearGradient
-                  colors={['rgba(12, 142, 194, 0.9)', 'rgba(51, 204, 128, 0.9)']}
-                  start={{ x: 0, y: 0 }}
-                  end={{ x: 1, y: 0 }}
-                  style={{ flex: 1 }}
-                />
+              return <MaterialCommunityIcons name={iconName} size={24} color={color} />;
+            },
+            tabBarLabelStyle: theme.textVariants.ui,
+            tabBarActiveTintColor: theme.colors.primary,
+            tabBarInactiveTintColor: theme.colors.inactive,
+            tabBarStyle: {
+              display: 'flex',
+              height: 54,
+              paddingVertical: 8,
+              paddingBottom: 4,
+              backgroundColor: '#fff',
+            },
+
+            headerTitleAlign: 'center',
+            headerTitle: (props) => (
+              <View style={{ flex: 1, justifyContent: 'center', alignSelf: 'center' }}>
                 <Image
-                  source={bgImage}
-                  style={{
-                    position: 'absolute',
-                    opacity: 0.6,
-                    zIndex: 0,
-                    top: 0,
-                    width: '100%',
-                    height: '100%',
-                  }}
+                  source={KuntaRekrySVG}
+                  resizeMode="contain"
+                  style={{ width: 215, height: 44 }}
                 />
               </View>
-            );
-          },
-        })}
-      >
-        <Tab.Screen name="Etusivu" component={HomeScreen} />
-        <Tab.Screen name="Suosikit" component={FavoritesScreen} />
-        <Tab.Screen name="Profiili" component={ProfileScreen} />
-        <Tab.Screen name="Ilmoitukset" component={AlertScreen} />
-        <Tab.Screen name="Lisää" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+            ),
+            headerBackground: () => {
+              return (
+                <View style={{ flex: 1 }}>
+                  <LinearGradient
+                    colors={['rgba(12, 142, 194, 0.9)', 'rgba(51, 204, 128, 0.9)']}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={{ flex: 1 }}
+                  >
+                    {/* <Image
+                      source={bgImage}
+                      style={{
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        resizeMode: 'cover',
+                      }}
+                    /> */}
+                  </LinearGradient>
+                </View>
+              );
+            },
+          })}
+        >
+          <Tab.Screen name="Etusivu" component={HomeScreen} />
+          <Tab.Screen name="Suosikit" component={FavoritesScreen} />
+          <Tab.Screen name="Profiili" component={ProfileScreen} />
+          <Tab.Screen name="Ilmoitukset" component={AlertScreen} />
+          <Tab.Screen name="Lisää" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
