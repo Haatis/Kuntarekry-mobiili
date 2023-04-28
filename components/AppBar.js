5fe6f1093e5e5551d1b460aa7c05cdb62e7029c8
n9heva00 <n9heva00@students.oamk.fi>
2023-04-28 12:57:11 +0300

    AppBar styling

--- components/AppBar.js
+++ components/AppBar.js
@@ -1,11 +1,12 @@
-import { View, Image, ImageBackground } from 'react-native';
+import { View, Image, ImageBackground, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import bgImage from '../assets/rajattu.png';
-import KuntaRekrySVG from '../assets/logo.png';
+import BackgroundImage from '../assets/substract.png';
+import Logo from '../assets/logo.png';
+import Constants from 'expo-constants';
 
 export default function AppBar() {
   return (
-    <View style={{ height: 80, flexDirection: 'row', overflow: 'hidden' }}>
+    <View style={styles.container}>
       <LinearGradient
         colors={['rgba(12, 142, 194, 0.9)', 'rgba(51, 204, 128, 0.9)']}
         start={{ x: 0, y: 0 }}
@@ -13,19 +14,38 @@ export default function AppBar() {
         style={{ flex: 1 }}
       >
         <ImageBackground
-          source={bgImage}
-          style={{ justifyContent: 'center', alignSelf: 'center' }}
+          source={BackgroundImage}
+          style={styles.imageContainer}
           imageStyle={{
-            flex: 1,
             height: '200%',
             resizeMode: 'repeat',
             opacity: 0.7,
-            transform: [{ rotateZ: '15deg' }],
+            transform: [{ rotateZ: '5deg' }, { scale: 1.5 }],
           }}
         >
-          <Image source={KuntaRekrySVG} resizeMode="contain" style={{ width: 215, height: 44 }} />
+          <Image
+            source={Logo}
+            resizeMode="contain"
+            style={{
+              marginTop: Constants.statusBarHeight,
+              width: '100%',
+              height: 44,
+            }}
+          />
         </ImageBackground>
       </LinearGradient>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: Constants.statusBarHeight + 52,
+    overflow: 'hidden',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
