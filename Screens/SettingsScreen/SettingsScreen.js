390da390a582f154e1c8571c8311f6675e27ff00
Haatis <joona00@windowslive.com>
2023-05-05 13:30:34 +0300

    async reset settings screen

--- Screens/SettingsScreen/SettingsScreen.js
+++ Screens/SettingsScreen/SettingsScreen.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { theme } from '../../styles/theme';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function SettingsScreen() {
   return (
     <View>
       <Text style={theme.textVariants.uiS}>Settings Screen</Text>
+      <Button onPress={() => AsyncStorage.clear()} title="Refresh async"></Button>
     </View>
   );
 }
