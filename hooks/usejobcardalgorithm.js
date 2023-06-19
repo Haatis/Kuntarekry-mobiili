9b62f9c17d7f4e2a4106ffa114a7674eb2bae101
Haatis <joona00@windowslive.com>
2023-06-19 12:31:34 +0300

    algorithm update

--- hooks/usejobcardalgorithm.js
+++ hooks/usejobcardalgorithm.js
@@ -1,25 +1,59 @@
 export default function useJobCardAlgorithm(jobs, userData) {
-  const userDataArray = Object.values(userData).map((value) => value.toString().toLowerCase());
+  console.log(userData);
+
+  const locationNamesArray = userData.locationNames
+    .map((location) => {
+      const name = location.name ? location.name.toString().toLowerCase() : null;
+      const parent = location.parent ? location.parent.toString().toLowerCase() : null;
+      return [name, parent].filter(Boolean);
+    })
+    .flat();
+
+  const taskNamesArray = userData.taskNames
+    .map((task) => {
+      const name = task.name ? task.name.toString().toLowerCase() : null;
+      const parent = task.parent ? task.parent.toString().toLowerCase() : null;
+      return [name, parent].filter(Boolean);
+    })
+    .flat();
+
+  const userDataArray = [
+    ...Object.values(userData).map((value) => {
+      if (typeof value === 'object' && value !== null && 'name' in value) {
+        return value.name.toString().toLowerCase();
+      }
+      return value.toString().toLowerCase();
+    }),
+    ...locationNamesArray,
+    ...taskNamesArray,
+  ];
+
+  const uniqueUserDataArray = [...new Set(userDataArray)];
+
   const jobsWithRanks = initRanks(jobs);
+
   jobsWithRanks.map((job) => {
-    userDataArray.forEach((userDataPart) => calculateRank(job, userDataPart));
-    //console.log(`Job: ${job.jobAdvertisement.title}, Rank: ${job.jobAdvertisement.rank}`);
+    uniqueUserDataArray.forEach((userDataPart) => calculateRank(job, userDataPart));
     return job;
   });
 
   const filteredJobs = jobsWithRanks.filter((j) => j.jobAdvertisement.rank !== 0);
   filteredJobs.sort((a, b) => b.jobAdvertisement.rank - a.jobAdvertisement.rank);
+
+  console.log('Rankings:');
+  filteredJobs.forEach((job) => {
+    console.log(`Job: ${job.jobAdvertisement.title}, Rank: ${job.jobAdvertisement.rank}`);
+  });
+
+  console.log('User Data Array:');
+  console.log(uniqueUserDataArray.join('\n'));
+
   return filteredJobs;
 }
-
 const filterFields = [
-  { name: 'title', rank: 20 },
   { name: 'location', rank: 20 },
-  { name: 'organization', rank: 20 },
   { name: 'region', rank: 10 },
   { name: 'taskArea', rank: 20 },
-  { name: 'employment', rank: 15 },
-  { name: 'employmentType', rank: 15 },
 ];
 
 const calculateRank = (job, userData) => {
